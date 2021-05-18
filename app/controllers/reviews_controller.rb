794b8fae13b7b02e7b5069586e781409bec80961
Peter Suh <psuh22@gmail.com>
2021-05-18 15:59:34 +0800

    newcode

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,7 +12,7 @@ class ReviewsController < ApplicationController
 
   def create
     # take info from the form and it to the database
-    @review = Review.new(params.require(:review).permit(:title, :body, :score))
+    @review = Review.new(form_params)
     @review.save
 
     redirect_to root_path
@@ -21,6 +21,32 @@ class ReviewsController < ApplicationController
   def show
     #individual review page
     @review = Review.find(params[:id])
+  end
+
+  def destroy
+    #find individual review
+      @review = Review.find(params[:id])
+    #destroy
+      @review.destroy
+    #redirect to the home page
+      redirect_to root_path
+  end
+
+  def edit
+
+      @review = Review.find(params[:id])
+  end
+
+  def update
+      @review = Review.find(params[:id])
+      @review.update(params.require(:review).permit(:title, :body, :score))
+      redirect_to review_path(@review)
+  end
+
+  def form_params
+
+    params.require(:review).permit(:title,:body,:score)
+
   end
 
 
